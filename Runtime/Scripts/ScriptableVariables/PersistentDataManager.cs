e8478070c76eb6e4e1411fee244d2ff20b43aa40
Simone Aronica <public.simonearonica@protonmail.ch>
2025-05-27 16:54:27 +0200

    Added events on save / load.

--- Runtime/Scripts/ScriptableVariables/PersistentDataManager.cs
+++ Runtime/Scripts/ScriptableVariables/PersistentDataManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Serialization;
 
 namespace Dev.Nicklaj.Butter
@@ -6,17 +7,30 @@ namespace Dev.Nicklaj.Butter
     public class PersistentDataManager : MonoBehaviour
     {
         public PersistentDataMasterList persistentDataMasterList;
+        
+        [Header("Events")]
+        [Tooltip("Event that will be raised when the game has finished loading.")]
+        public GameEvent GameFinishedLoading;
+        [Tooltip("Event that will be raised when the game has finished saving.")]
+        public GameEvent GameFinishedSaving;
+
+        public UnityAction OnLoadingFinished;
+        public UnityAction OnSavingFinished;
 
         public void SaveData()
         {
             if (persistentDataMasterList == null) return;
             persistentDataMasterList.SaveData();
+            GameFinishedSaving?.Raise();
+            OnSavingFinished?.Invoke();
         }
 
         public void LoadData()
         {
             if (persistentDataMasterList == null) return;
             persistentDataMasterList.LoadData();
+            GameFinishedLoading?.Raise();
+            OnLoadingFinished?.Invoke();
         }
     }
 }
